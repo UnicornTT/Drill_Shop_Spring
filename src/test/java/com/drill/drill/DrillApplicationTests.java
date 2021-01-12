e4e77daff52438240e982aba607c46e7a242ff8b
UnicornTT <45918385+UnicornTT@users.noreply.github.com>
2021-01-12 20:00:15 +0300

    Add files via upload

---
+++ src/test/java/com/drill/drill/DrillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drill.drill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
